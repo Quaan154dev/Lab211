e72bbf3bfbd1467d0acafebf109ab57355965ea7
Đào Bá Anh Quân <113249943+Quaan154dev@users.noreply.github.com>
2023-11-04 16:32:48 +0700

    commit

---
+++ 0076-CSV/src/Main.java
@@ -0,0 +1,18 @@
+
+import Controller.CSVcontroller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author quandba
+ */
+public class Main {
+    public static void main(String[] args) {
+        CSVcontroller csv= new CSVcontroller();
+        csv.run();
+    }
+}
---
+++ 0079-ZIP/src/Controller/FileCompressController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+import Responsitory.FileCompressionRES;
+import View.Menu;
+/**
+ *
+ * @author quandba
+ */
+public class FileCompressController extends Menu {
+     private FileCompressionRES fileCompressionRES;
+    static String[] option = {"Compression", "Extraction", "Exit"};
+
+    public FileCompressController() {
+        super("========= Zipper program =========", option);
+        fileCompressionRES = new FileCompressionRES();
+    }
+
+    @Override
+    public void execute(int choice) {
+        switch(choice){
+            case 1:
+                fileCompressionRES.Zip_file();
+                break;
+            case 2:
+                fileCompressionRES.Unzip_file();
+                break;
+            case 3:
+                System.out.println("Exit!!!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid!!!");
+        }
+    }
+    
+}
